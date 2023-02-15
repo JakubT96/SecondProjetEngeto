03ec53131cf58784c205aa3b7db6c7c3d57cbd90
Jakub Toman <JakubT96@seznam.cz>
2023-02-15 22:17:03 +0100

    Some other adjustments. Postman update. All controllers are functional from postman. First submission to https://learn.engeto.com/cs/

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -3,42 +3,38 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.Collection;
 
 @RestController
 public class ProductController {
 
-
-    @GetMapping("/hello")      //FUNGUJE
+    @GetMapping("/hello")
     public String hello() {
         return "Aplikace druhého projetu ENGETO!";
     }
 
-    @GetMapping("/allProduct")     //FUNGUJE
+    @GetMapping("/allProduct")
     public Collection <Product> loadAllAvailableItems()throws SQLException{
         return ProductService.LoadAllAvailableItems();
     }
 
-
     @GetMapping ("/productID/{id}")
-    public Product loadProductById (@PathVariable(value = "id") int id) throws SQLException{
+    public Product loadProductById (@PathVariable(value ="id") int id) throws SQLException{
         return ProductService.LoadProductByID(id);
     }
 
-
     @PostMapping("/SaveProduct")
     public Product saveItem (@RequestBody Product product) throws SQLException{
-        return ProductService. saveItem(product);
+        return ProductService.saveItem(product);
     }
 
-
-    @PutMapping("/UpdateProduct/{id}/{price}")
-    public void updatePriceByID(@PathVariable(value = "id") int id, @RequestParam(value = "price") BigDecimal price) throws SQLException {
+    @PostMapping("/UpdateProduct/{id}/{price}")
+    public void updatePriceByID(@PathVariable(value = "id") int id,@PathVariable(value = "price") BigDecimal price) throws SQLException {
        ProductService.UpdatePriceByID(id, price);
     }
 
-
-    @DeleteMapping ("/DeleteProduct")     //FUNGUJE
+    @DeleteMapping ("/DeleteProduct")
     public void deleteOutOfSaleItem ()throws SQLException{
     ProductService.DeleteOutSaleItems();
 }
--- src/main/java/com/example/demo/ProductService.java
+++ src/main/java/com/example/demo/ProductService.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.mysql.cj.jdbc.ConnectionImpl;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -8,7 +7,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Scanner;
+
 @Service
 
 public class ProductService {
@@ -39,9 +38,7 @@ public class ProductService {
         }
     }
 
-
-    // 1.Vrací seznam všech druhů zboží v evidenci
-    static Collection<Product> LoadAllAvailableItems() throws SQLException {    //FUNGUJE
+    static Collection<Product> LoadAllAvailableItems() throws SQLException {
         statement = connnection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM product");
             List<Product> actualProductList = new ArrayList<>();
@@ -51,34 +48,28 @@ public class ProductService {
             }
         return actualProductList;
     }
-    private static Product extractItemData(ResultSet resultSet) throws SQLException {
-
+    private static Product extractItemData(ResultSet resultSet)  throws SQLException{
         return new Product(
                 resultSet.getInt(1),
                 resultSet.getInt(2),
                 resultSet.getString(3),
                 resultSet.getString(4),
                 resultSet.getBoolean(5),
-                resultSet.getBigDecimal(6));
+                resultSet.getBigDecimal(6)
+        );
     }
 
-
-    // 2. Vrátí informaci o produktu se zadaným ID
        public static Product LoadProductByID(int id) throws SQLException {
            statement = connnection.createStatement();
-           ResultSet resultSet = statement.executeQuery("SELECT * FROM products WHERE id = "+id);
+           ResultSet resultSet = statement.executeQuery("SELECT * FROM product WHERE id = "+id+"");
            resultSet.next();
-           return extractItemData(resultSet);
+           return  extractItemData(resultSet);
        }
-
-
-    //3. PŘIDÁNÍ POLOŽKY
 
     public static Product saveItem(Product product) throws SQLException {
         statement = connnection.createStatement();
-        String sqlString =  "INSERT INTO products(id, partNumber,name,description,isForSale,price) " +
-                "VALUES ('"+product.getId()+"','"+product.getPartNumber()+"','"+product.getName()+"',"+product.getDescription()+","
-                +product.getForSale()+","+product.getPrice()+")";
+        String sqlString = "INSERT INTO product(id,partNumber,name,description,isForSale,price) " +
+                "VALUES ('"+product.getId()+"','"+product.getPartNumber()+"','"+product.getName()+"','"+product.getDescription()+"',"+product.getForSale()+","+product.getPrice()+")";
         statement.executeUpdate(sqlString , Statement.RETURN_GENERATED_KEYS);
 
         ResultSet generatedKeys = statement.getGeneratedKeys();
@@ -87,16 +78,13 @@ public class ProductService {
         return product;
     }
 
-    //4. AKTULIZACE ZADANÉ CENY PODLE ZADANÉHO ID
     public static void UpdatePriceByID(int id, BigDecimal price)  throws SQLException {
         statement = connnection.createStatement();
-        String sql1 = "UPDATE products SET price="+price+" WHERE id="+id;
+        String sql1 = "UPDATE product SET price = "+price+" WHERE id = "+id+" ";
         statement.executeUpdate(sql1);
     }
 
-
-    //5. SMAZÁNÍ PODLE isForSle
-    static void DeleteOutSaleItems() throws SQLException {     //FUNGUJE
+    static void DeleteOutSaleItems() throws SQLException {
         statement = connnection.createStatement();
         String sql2 = "DELETE FROM product WHERE isForSale = '0' ";
         statement.executeUpdate(sql2);
